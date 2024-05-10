2599e079a9a795bc0cc1c0ae324ae38d14026b90
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-05-10 20:16:38 +0600

    created delete function

--- index.js
+++ index.js
@@ -88,6 +88,14 @@ async function run() {
 
     })
 
+    // delete food
+    app.delete('/foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodsCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
     // Send a ping to confirm a successful connection
